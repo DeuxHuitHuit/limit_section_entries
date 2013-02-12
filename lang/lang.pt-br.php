624b93f85acb739d2dc4e838391f9bb07bb5e4c2
Marcio Toledo <mt@marciotoledo.com>
2013-02-11 20:59:19 -0800

    Add Portuguese (Brazil) language

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,42 @@
+<?php
+
+$about = array(
+  'name' => 'Portuguese (Brazil)',
+	'author' => array(
+		'name' => 'Marcio Toledo',
+		'email' => 'mt@marciotoledo.com',
+		'website' => 'http://marciotoledo.com'
+		),
+	'release-date' => '2013-02-12'
+	);
+
+$dictionary = array(
+
+	'There is %d entry' => 
+	'Há %d registro',
+
+	'There are %d entries' => 
+	'Há %d registros',
+
+	' out of a maximum of ' => 
+	' de um máximo de ',
+
+	'You can\'t create more entries.' => 
+	'Você não pode criar mais registros.',
+
+	'You can create %d more' => 
+	'Você pode criar mais %d',
+
+	'entry' => 
+	'registro',
+
+	'entries' => 
+	'registros',
+
+	'Maximum entries' => 
+	'Máximo de registros',
+
+	'Limit the maximum number of entries to this positive integer value. Let 0 or empty for unlimited.' => 
+	'Limite o número máximo de registros para um valor inteiro positivo. Deixe 0 ou vazio para ilimitado.',
+
+	);
